1bc945baefc6b86f25b37a82642aff74809ba265
chenanqi <527078124@qq.com>
2021-03-15 02:32:07 +0800

    首页功能分支先提交一下

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
 $(function () {
-
+    getUser()
 })
+//获取用户信息这个功能其他页面也要用
+//封装成函数并且定义全局变量
+function getUser() {
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        headers:localStorage.getItem('token'),
+        data: {},
+        success: (res) => {
+            console.log(res);
+        }
+    })
+}
